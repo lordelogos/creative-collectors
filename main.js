c8b820ebb6b7d928e9bfe32e20a42445769856aa
Paul OE <paulehiks@gmail.com>
2022-03-14 23:52:31 +0100

    fixed custom scrollbar v0.0.6

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-let el = document.querySelector(".nft-showcase");
+let _el = document.querySelector(".nft-showcase");
 
-//setup custom cursor
-// let cursor = document.querySelector('.cursor');
 
-// window.addEventListener('mousemove', (e) => {
-// 	cursor.setAttribute('style', `top: ${e.clientY}px; left: ${e.clientX}px`);
-// });
 
 // initializing nft showcase
-const glider = new Glider(el, {
+const glider = new Glider(_el, {
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	// exactWidth: true,
@@ -99,17 +94,17 @@ function startTimeout() {
 
 let animID = 0;
 const isAnimating = () => glider.animate_id !== animID;
-el.addEventListener("glider-animated", () => {
+_el.addEventListener("glider-animated", () => {
 	animID = glider.animate_id;
 	if (!hovering) startTimeout();
 });
 
-el.addEventListener("mouseover", () => {
+_el.addEventListener("mouseover", () => {
 	hovering = true;
 	clearTimeout(timeout);
 });
 
-el.addEventListener("mouseout", () => {
+_el.addEventListener("mouseout", () => {
 	hovering = false;
 	if (!isAnimating()) startTimeout();
 });
@@ -136,7 +131,7 @@ gsap.registerPlugin("ScrollTrigger");
 
 // setting up locomotive scroll
 const locoScroll = new LocomotiveScroll({
-	el: document.querySelector(".smooth-scroll"),
+	el: document.querySelector("[data-scroll-container]"),
 	smooth: true,
 	smartphone: {
 		smooth: true,
@@ -365,3 +360,102 @@ window.addEventListener("load", () => {
 		locoScroll.start();
 	}, 3000);
 });
+
+//setting up custom scroll
+//setup custom cursor
+// let cursor = document.querySelector('.cursor');
+
+// window.addEventListener('mousemove', (e) => {
+// 	cursor.setAttribute('style', `top: ${e.clientY}px; left: ${e.clientX}px`);
+// });
+
+//utility functions
+const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+// Gets the mouse position
+const getMousePos = e => {
+  let posx = 0;
+  let posy = 0;
+  if (!e) e = window.event;
+  if (e.pageX || e.pageY) {
+    posx = e.pageX;
+    posy = e.pageY;
+  } else if (e.clientX || e.clientY) {
+    posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
+    posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
+  }
+
+  return { x: posx, y: posy };
+};
+
+//cursor & helper functions
+let mouse = { x: 0, y: 0 };
+if (typeof window !== "undefined") {
+  window.addEventListener("mousemove", ev => (mouse = getMousePos(ev)));
+}
+
+class Cursor {
+  constructor(el) {
+    this.DOM = { el: el };
+    this.DOM.el.style.opacity = 0;
+
+    this.bounds = this.DOM.el.getBoundingClientRect();
+
+    this.renderedStyles = {
+      tx: { previous: 0, current: 0, amt: 0.15 },
+      ty: { previous: 0, current: 0, amt: 0.15 },
+      scale: { previous: 1, current: 1, amt: 0.15 },
+      opacity: { previous: 1, current: 1, amt: 0.1 }
+    };
+
+    this.onMouseMoveEv = () => {
+      this.renderedStyles.tx.previous = this.renderedStyles.tx.current =
+        mouse.x - this.bounds.width / 2;
+      this.renderedStyles.ty.previous = this.renderedStyles.ty.previous =
+        mouse.y - this.bounds.height / 2;
+      gsap.to(this.DOM.el, {
+        duration: 0.9,
+        ease: "Power3.easeOut",
+        opacity: 1
+      });
+      requestAnimationFrame(() => this.render());
+      window.removeEventListener("mousemove", this.onMouseMoveEv);
+    };
+    window.addEventListener("mousemove", this.onMouseMoveEv);
+  }
+  enter() {
+    this.renderedStyles["scale"].current = 2.5;
+    this.renderedStyles["opacity"].current = 0.5;
+  }
+  leave() {
+    this.renderedStyles["scale"].current = 1;
+    this.renderedStyles["opacity"].current = 1;
+  }
+  render() {
+    this.renderedStyles["tx"].current = mouse.x - this.bounds.width / 2;
+    this.renderedStyles["ty"].current = mouse.y - this.bounds.height / 2;
+
+    for (const key in this.renderedStyles) {
+      this.renderedStyles[key].previous = lerp(
+        this.renderedStyles[key].previous,
+        this.renderedStyles[key].current,
+        this.renderedStyles[key].amt
+      );
+    }
+
+    this.DOM.el.style.transform = `translateX(${this.renderedStyles["tx"].previous}px) translateY(${this.renderedStyles["ty"].previous}px) scale(${this.renderedStyles["scale"].previous})`;
+    this.DOM.el.style.opacity = this.renderedStyles["opacity"].previous;
+
+    requestAnimationFrame(() => this.render());
+  }
+}
+
+const cursor = new Cursor(document.querySelector('.cursor'));
+[...document.querySelectorAll(".li-link")].forEach(link => {
+	link.addEventListener("mouseenter", () => cursor.enter());
+	link.addEventListener("mouseleave", () => cursor.leave());
+  });
+  [...document.querySelectorAll("button")].forEach(link => {
+	link.addEventListener("mouseenter", () => cursor.enter());
+	link.addEventListener("mouseleave", () => cursor.leave());
+  });
